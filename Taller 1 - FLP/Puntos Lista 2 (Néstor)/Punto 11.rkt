82cdf4745e8ef374a76018fdf461c379721c56fa
nestor711 <114924593+nestor711@users.noreply.github.com>
2023-09-10 09:20:57 -0500

    Add files via upload

---
+++ Taller 1 - FLP/Puntos Lista 2 (Néstor)/Punto 11.rkt
@@ -0,0 +1,19 @@
+#lang racket
+
+;; zip :
+;; Proposito:
+;; S x L -> L’ : Procedimiento que remueve la primera
+;; ocurrencia de un simbolo S en una lista de simbolos L.
+; <zip> ::= zip(<funcion>, <lista1>, <lista2>)
+
+(define (zip F L1 L2)
+  (if (or (null? L1) (null? L2))
+      '()
+      (cons (F (car L1) (car L2))
+            (zip F (cdr L1) (cdr L2)))))
+
+; Pruebas
+;> (zip + ’(1 4) ’(6 2))
+;(7 6)
+;> (zip * ’(11 5 6) ’(10 9 8))
+;(110 45 48)
