abb4f028647b2e0baec7845635a53ab704d21486
nestor711 <114924593+nestor711@users.noreply.github.com>
2023-09-10 08:47:35 -0500

    Add files via upload

---
+++ Taller 1 - FLP/Puntos Lista 2 (Néstor)/Punto 2.rkt
@@ -0,0 +1,24 @@
+#lang racket
+
+;; down :
+;; Proposito:
+;; down L -> Lista Función que debe retornar una lista con cada elemento de L asociado a un nivel más de paréntesis comparado con su estado original en L.
+;;
+;;<lista> := ()
+;; := (<valor-de-scheme> <lista>)
+
+(define (down L) ; Se define la función down con argumento una lista L
+  (cond 
+    [(empty? L) '()]           ; Si la lista es vacía, retorna una lista vacía.
+    [else (cons (list (car L)) ; Crea una lista con el primer elemento de L.
+                (down (cdr L))) ; Llama a down recursivamente con el resto de la lista.
+          ]))
+
+;; Pruebas
+;; > (down ’(1 2 3))
+;;((1) (2) (3))
+;;> (down ’((una) (buena) (idea)))
+;;(((una)) ((buena)) ((idea)))
+;;> (down ’(un (objeto (mas)) complicado))
+;;((un) ((objeto (mas))) (complicado))
+
